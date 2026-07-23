8ef5d7ace0f0b72aee99e94b2107d6d25806da9c
BungerColumbus <72514150+BungerColumbus@users.noreply.github.com>
2026-07-23 16:08:41 +0200

    annoying popups

--- Assets/Scripts/SpawnPopUps.cs
+++ Assets/Scripts/SpawnPopUps.cs
@@ -1,15 +1,41 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpawnPopUps : MonoBehaviour
 {
-    [SerializeField] 
-    private GameObject[] popUpPrefab;
-    private GameObject popUp;
-    private Vector3 position_start;
-    private Vector3 position_end;
+    [Header("Settings")]
+    [SerializeField] private GameObject[] popUpPrefabs;
+    [SerializeField] private int[] x = new int[2];
+    [SerializeField] private int[] y = new int[2];
+    
+    [Header("Timing")]
+    [SerializeField] private float minSpawnTime = 4f;
+    [SerializeField] private float maxSpawnTime = 6f;
+    
+    private bool canSpawn = true;
+
     void Update()
     {
-        popUp = Instantiate(popUpPrefab[Random.Range(0, popUpPrefab.Length)]);
-        popUp.transform.position = new Vector3();
+        if (canSpawn)
+        {
+            StartCoroutine(SpawnPopUpRoutine());
+        }
+    }
+
+    IEnumerator SpawnPopUpRoutine()
+    {
+        canSpawn = false;
+        
+        GameObject newPopUp = Instantiate(popUpPrefabs[Random.Range(0, popUpPrefabs.Length)], transform, false);
+        
+        RectTransform rectTransform = newPopUp.GetComponent<RectTransform>();
+        
+        Vector2 randomTargetPos = new Vector2(Random.Range(x[0], x[1]), Random.Range(y[0], y[1])); 
+        rectTransform.anchoredPosition = randomTargetPos;
+        
+        float waitTime = Random.Range(minSpawnTime, maxSpawnTime);
+        yield return new WaitForSeconds(waitTime);
+        
+        canSpawn = true;
     }
 }
